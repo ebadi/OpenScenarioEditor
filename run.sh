9ea79917ac8dae5047c7cc5824746ba58420b905
Hamid Ebadi <hamid.ebadi@gmail.com>
2021-02-11 12:44:36 +0100

    fix dev repo

--- run.sh
+++ run.sh
@@ -4,8 +4,9 @@ then
     sudo apt install git python3-pip python3-pyqt5 # first run, install missing packages
     pip3 install PyQt5
     git clone git@github.com:ebadi/pyesmini.git pyesmini  # dev repo
-    git checkout dev # dev branch
-    cd pyesmini ; ./build.sh
+    cd pyesmini
+    git checkout dev
+    ./build.sh
     cd ..
 fi
 python3 OpenScenarioEditor.py
