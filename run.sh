c2b8ff04837f23a8c6f73c366460565b6e133302
Hamid Ebadi <hamid.ebadi@gmail.com>
2021-02-11 12:40:27 +0100

    dev repo

--- run.sh
+++ run.sh
@@ -3,7 +3,8 @@ if [ ! -d "pyesmini" ]
 then
     sudo apt install git python3-pip python3-pyqt5 # first run, install missing packages
     pip3 install PyQt5
-    git clone https://github.com/ebadi/pyesmini pyesmini
+    git clone git@github.com:ebadi/pyesmini.git pyesmini  # dev repo
+    git checkout dev # dev branch
     cd pyesmini ; ./build.sh
     cd ..
 fi
